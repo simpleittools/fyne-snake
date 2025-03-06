28f61100c74630773f77269eae616bcbe5bce461
Ryan <wm.ryan.mooney@gmail.com>
2025-03-06 13:37:24 -0900

    added the timer to move the snake on screen

--- main.go
+++ main.go
@@ -6,6 +6,17 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"image/color"
+	"time"
+)
+
+// snakePart is the game state
+type snakePart struct {
+	x, y float32
+}
+
+var (
+	snakeParts []snakePart
+	game       *fyne.Container
 )
 
 func setupGame() *fyne.Container {
@@ -16,15 +27,41 @@ func setupGame() *fyne.Container {
 		r.Resize(fyne.NewSize(10, 10))
 		r.Move(fyne.NewPos(90, float32(50+i*10)))
 		segments = append(segments, r)
+
+		seg := snakePart{
+			9, float32(5 + i),
+		}
+		snakeParts = append(snakeParts, seg)
 	}
 	return container.NewWithoutLayout(segments...)
+}
+
+func refreshGame() {
+	for i, seg := range snakeParts {
+		rect := game.Objects[i]
+		rect.Move(fyne.NewPos(seg.x*10, seg.y*10))
+	}
+	game.Refresh()
+}
+
+func runGame() {
+	for {
+		time.Sleep(time.Millisecond * 250)
+		for i := len(snakeParts) - 1; i >= 1; i-- {
+			snakeParts[i] = snakeParts[i-1]
+		}
+		snakeParts[0].y--
+		refreshGame()
+	}
 }
 
 func main() {
 	a := app.New()
 
 	w := a.NewWindow("Snake")
-	w.SetContent(setupGame())
+	game = setupGame()
+	w.SetContent(game)
+	go runGame()
 	w.Resize(fyne.NewSize(200, 200))
 	w.SetFixedSize(true)
 	w.ShowAndRun()
